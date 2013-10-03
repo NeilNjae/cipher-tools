e354c8aa351b8b9c0d9f28aa5708883c209809fe
Neil Smith <neil.github@njae.me.uk>
2013-10-03 11:58:35 +0100

    Initial import

---
+++ cipher.py
@@ -0,0 +1,23 @@
+import string
+
+
+def caesar_cipher_letter(letter, shift):
+    if letter in string.ascii_letters:
+        if letter in string.ascii_lowercase:
+            return chr((ord(letter) - ord('a') + shift) % 26 + ord('a'))
+        else:
+            new_letter = letter.lower()
+            yolo = chr((ord(new_letter) - ord('a') + shift) % 26 + ord('a'))
+            return yolo.upper()
+    else:
+        return letter
+
+def caesar_decipher_letter(letter, shift):
+    return caesar_cipher_letter(letter, -shift)
+
+def caesar_cipher_message(message, shift):
+    big_cipher = [caesar_cipher_letter(l, shift) for l in message]
+    return ''.join(big_cipher)
+
+def caesar_decipher_message(message, shift):
+    return caesar_cipher_message(message, -shift)
